aa771e233aa30590a2d9bd58aca9e71a1fc480c3
PDParla <pd.parla@alumnos.urjc.es>
2019-03-14 11:48:32 -0400

    clase 14 marzo

--- pruebaMSER.py
+++ pruebaMSER.py
@@ -1,21 +1,23 @@
+
 import numpy as np
 import cv2 as cv
 import sys
-from os import listdir
 
 def cogerImagen(name_img):
     try:
        img = cv.imread(name_img)
-       return img 
+       return img
     except Exception:
         print('No se puede cargar la imagen,saliendo')
         sys.exit(-1)
 
-if __name__ == '__main__':
-    for imagen in os.listdir('./train_10_ejemplos'):
-        if 'ppm' in imagen:
-            img = cogerImagen(imagen)
+def hazmeCuadrado(rectangulo):
+    x = rectangulo[0] - 15
+    y = rectangulo[1] - 15
+    return np.array([x,y,50,50])
 
+if __name__ == '__main__':
+    img = cogerImagen('./train_10_ejemplos/00006.ppm')
     mser = cv.MSER_create()
 
     while True:
@@ -26,11 +28,11 @@ if __name__ == '__main__':
         regions, x  = mser.detectRegions(gray)
         for rectangle in x:
             # Por cada region encontrada, pintamos su rectangulo
-            if (rectangle[2]/rectangle[3] >= 0.8) and (rectangle[2]/rectangle[3]
-                                                      <= 1.2):
-                cv.rectangle(vis, (rectangle[0],rectangle[1]),
-                         (rectangle[0]+rectangle[2],rectangle[1]+rectangle[3]),
-                         (0, 255, 0))
+     #       if (rectangle[2]/rectangle[3] >= 0.65) and (rectangle[2]/rectangle[3] <= 1.35):
+                cuadrado = hazmeCuadrado(rectangle)
+                cv.rectangle(vis,(cuadrado[0],cuadrado[1]),
+                             (cuadrado[0] + cuadrado[2],cuadrado[1] +
+                              cuadrado[3]),(0,255,0))
 
         cv.imshow('img', vis)
         if cv.waitKey(5) == 27:
