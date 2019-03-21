9725a17cb95afda6ab4e13863399cdbbe17f5da3
PDParla <pd.parla@alumnos.urjc.es>
2019-03-21 12:23:05 -0400

    Programa mejorado

--- pruebaMSER.py
+++ pruebaMSER.py
@@ -1,7 +1,11 @@
-
 import numpy as np
 import cv2 as cv
-import sys
+import sys,os
+
+#ZONA DEFINICIÓN DE CONSTANTES
+MAX_DIVISION = 1.15
+MIN_DIVISION = 0.85
+
 
 def cogerImagen(name_img):
     try:
@@ -12,29 +16,62 @@ def cogerImagen(name_img):
         sys.exit(-1)
 
 def hazmeCuadrado(rectangulo):
-    x = rectangulo[0] - 15
-    y = rectangulo[1] - 15
-    return np.array([x,y,50,50])
+    x = int(round(rectangulo[0] * 0.99))
+    y = int(round(rectangulo[1] * 0.98))
+    w = rectangulo[2]
+    h = rectangulo[3]
+    if w > h:
+        w = int(round(w * 1.25)) 
+        h = w
+    else:
+        h = int(round(h * 1.25))
+        w = h
+    return np.array([x,y,w,h])
+
+
+# Mejor contraste en imagen: sacado de https://stackoverflow.com/questions/39308030/how-do-i-increase-the-contrast-of-an-image-in-python-opencv
+def mejorContraste(img):
+    #-----Converting image to LAB Color model----------------------------------- 
+    lab= cv.cvtColor(img, cv.COLOR_BGR2LAB)
+
+    #-----Splitting the LAB image to different channels-------------------------
+    l, a, b = cv.split(lab)
+
+    #-----Applying CLAHE to L-channel-------------------------------------------
+    clahe = cv.createCLAHE(clipLimit=2.5, tileGridSize=(8,8))
+    cl = clahe.apply(l)
+
+    #-----Merge the CLAHE enhanced L-channel with the a and b channel-----------
+    limg = cv.merge((cl,a,b))
+
+    #-----Converting image from LAB Color model to RGB model--------------------
+    final = cv.cvtColor(limg, cv.COLOR_LAB2BGR)
+    return final
 
 if __name__ == '__main__':
-    img = cogerImagen('./train_10_ejemplos/00006.ppm')
-    mser = cv.MSER_create()
 
-    while True:
-        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        vis = img.copy()
-        # Cogemos las regiones detectadas y sus arrays de coordenadas x,y
-        # anchura y altura en ese orden
-        regions, x  = mser.detectRegions(gray)
-        for rectangle in x:
-            # Por cada region encontrada, pintamos su rectangulo
-     #       if (rectangle[2]/rectangle[3] >= 0.65) and (rectangle[2]/rectangle[3] <= 1.35):
-                cuadrado = hazmeCuadrado(rectangle)
-                cv.rectangle(vis,(cuadrado[0],cuadrado[1]),
-                             (cuadrado[0] + cuadrado[2],cuadrado[1] +
-                              cuadrado[3]),(0,255,0))
 
-        cv.imshow('img', vis)
-        if cv.waitKey(5) == 27:
-            break
+    for imagen in os.listdir('./train_10_ejemplos/'):
+        img = cogerImagen(f'./train_10_ejemplos/{imagen}')
+        imgcontraste = mejorContraste(img)
+        mser = cv.MSER_create(_delta = 7, _max_area =20000,
+                              _max_variation = .15)
+
+        while True:
+            gray = cv.cvtColor(imgcontraste, cv.COLOR_BGR2GRAY)
+            # Cogemos las regiones detectadas y sus arrays de coordenadas x,y
+            # anchura y altura en ese orden
+
+            regions, x  = mser.detectRegions(gray)
+            vis = img.copy()
+            for region,rectangle in zip(regions,x):
+                breakpoint();
+                # Por cada region encontrada, pintamos su rectangulo
+                if (rectangle[2]/rectangle[3] >= MIN_DIVISION) and (rectangle[2]/rectangle[3] <= MAX_DIVISION):
+                    x,y,w,h = hazmeCuadrado(rectangle)
+                    cv.rectangle(vis,(x,y),(x + w, y + h),(0,255,0))
+
+            cv.imshow('img', vis)
+            if cv.waitKey(5) == 27:
+                break
 cv.destroyAllWindows()
