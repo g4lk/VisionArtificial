2bd46bd9d5f0ef683f3332c068ca319b5699000c
g4lk <pd.parla@alumnos.urjc.es>
2019-04-09 03:35:17 +0200

    Casi completo

---
+++ ventana.py
@@ -0,0 +1,16 @@
+
+class Ventana:
+    def __init__(self,img,x,y,w,h,type,score):
+        self.img = img
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.type = type
+        self.score = score
+
+    def str(self):
+        return f'{self.img};{self.x};{self.y};{self.w};{self.h};1;{str(self.score)}\n'
+
+    def str_tipo(self):
+        return f'{self.img};{self.x};{self.y};{self.w};{self.h};{self.type};{str(self.score)}\n'
